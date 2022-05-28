ef5dc6e8c74318432dbfb5941b1cf5525f77ba9b
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-05-28 19:42:32 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+let student:string = "Nahid";
+let age:number = 57;
+let isSmart:boolean = true;
+
+let students:string[] = ["Korim", "Rohim", "Hamid"];
+let feeds:number[] = [12, 25, 36, 75, 87];
+
+
+interface Person {
+  name:string,
+  age:number,
+  job?:string,
+  location?: string | number
+  employed?: any
+}
+
+type users = {
+  name:string,
+  age:number,
+  job?:string,
+  location?: string | number
+  employed?: any
+}
+
+const person: Person = {
+  name: "nahid",
+  age: 23,
+  job: "Jobless",
+  location: 55
+}
+
+const derson: Person = {
+  name: "nahid",
+  age: 23,
+}
+
+// const person: {name:string, age:number, job:string} = {
+//   name: "nahid",
+//   age: 23,
+//   job: "Jobless" 
+// }
+
+
+const handleAddUser = (firstName:string, age:number, address:string) => {
+  const total:number = 50;
+  console.log(firstName, age, address);
+  return total
+}
+
+const handleUser = (firstName:string, age:number, address:string):void => {
+  const total:number = 50;
+  console.log(firstName, age, address);
+  // return total
+}
+
+const handleage = (firstName:string, age:number, address:string):string => {
+  const total:number = 50;
+  console.log(firstName, age, address);
+  return address
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
---
+++ src/vanila/vanila.js
@@ -0,0 +1,8 @@
+let user = 'Robert';
+user = 'korim';
+user = 57;
+user = {name: "nahid", age: 23, address: "Dhaka"}
+user = [1, 5, 6, 7, 9, 12];
+user = ["korim", "Rohim", "Abul"]
+user = () => 79;
+console.log(user);
